b415ea02dc89c6a194bc144df267d8487ce13836
Digiraj Solutions <digiraj.in@gmail.com>
2021-04-24 20:46:00 +0530

    Implemented prototype inheritance.

--- js/12-protoType.js
+++ js/12-protoType.js
@@ -21,6 +21,7 @@ let person1 = new Person('Rohan');
 console.log(person1);
 */
 
+/*
 function Student() {
   this.name = 'Rohit';
   this.gender = 'Male';
@@ -34,3 +35,56 @@ Student.prototype.sayHi = function () {
 let stud1 = new Student();
 console.log(stud1);
 console.log(stud1.sayHi());
+*/
+
+const proto = {
+  slogan: function () {
+    return `This company is the best`;
+  },
+  changeName: function (newName, newRole) {
+    this.name = newName;
+    this.role = newRole;
+  },
+};
+
+// This creates rohit object inheriting from object proto
+let rohit = Object.create(proto);
+rohit.name = 'rohit';
+rohit.role = 'Programmer';
+console.log(rohit);
+
+rohit.changeName('Raj', 'Marketer');
+console.log(rohit);
+
+// Executing Prototype inheritance
+// Employee constructor
+function Employee(name, salary, experience) {
+  this.name = name;
+  this.salary = salary;
+  this.experience = experience;
+}
+
+// Slogan prototype
+Employee.prototype.slogan = function () {
+  return `This company is the best. Regards, ${this.name}`;
+};
+
+// Create an object from this constructor now
+let rohitObj = new Employee('Rohit', 50000, 12);
+console.log(rohitObj);
+console.log(rohitObj.slogan());
+
+// Programmer
+function Programmer(name, salary, experience, language) {
+  Employee.call(this, name, salary, experience);
+  this.language = language;
+}
+
+// Inherit the prototype
+Programmer.prototype = Object.create(Employee.prototype);
+
+// Manually set the constructor
+Programmer.prototype.constructor = Programmer;
+
+let rohan = new Programmer('Rohan', 2, 0, 'Javascript');
+console.log(rohan);
